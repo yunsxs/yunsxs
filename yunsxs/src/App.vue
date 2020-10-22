74ac3b8dccc008d9314508565fa09b278794d7b3
朱霁明 <2691066836@qq.com>
2020-10-22 15:45:42 +0800

    首页

--- yunsxs/src/App.vue
+++ yunsxs/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -16,8 +15,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  margin: 0px;
+  padding: 0px;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
