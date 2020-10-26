48cd3ebe0e4212c2520510d1a1c1050e83a15821
hashihui <2732542552@qq.com>
2020-10-26 09:49:20 +0800

    静态页面

--- yunsxs/src/router/index.js
+++ yunsxs/src/router/index.js
@@ -6,23 +6,33 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path:'/index',
-      name:indexedDB,
-      component:resolve=>require(["@/views/index/index"],resolve),
+      path: '/index',
+      name: indexedDB,
+      component: resolve => require(["@/views/index/index"], resolve),
     },
     {
-      path: '',
-      redirect: "/home",
+      path: '/home',
+      redirect: "/government",
       name: 'Home',
       meta: {
         requireAuth: true
       },
-      component:resolve=>require(["@/views/Home/Home"],resolve),
-      children:[
+      component: resolve => require(["@/views/Home/Home"], resolve),
+      children: [
         {
-          path: '/home',
-          name: 'home',
-          component:resolve=>require(["@/views/Home/Home"],resolve),
+          path: '/government',
+          name: 'Government',
+          component: resolve => require(["@/views/Home/components/Government"], resolve),
+        },
+        {
+          path: '/photograph',
+          name: 'photograph',
+          component: resolve => require(["@/views/Home/components/Photograph"], resolve)
+        },
+        {
+          path: '/videoNews',
+          name: 'videoNews',
+          component: resolve => require(['@/views/Home/components/VideoNews'], resolve)
         }
       ]
     }
